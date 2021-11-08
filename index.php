9a1fdf7db1600e64d693c9f96715f8b4fc29bbbd
Komla Adzam <adzamkomla.dev@gmail.com>
2021-11-08 14:55:00 +0000

    Add sayName function

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ function sayHello(string $name = "Sarah Mensah"): string {
     return "Hello {$name}";
 }
 
+function sayName(string $name): string {
+    return "Hey {$name}";
+}
+
 echo sayHello();
+echo sayName("Komla Adzam");
